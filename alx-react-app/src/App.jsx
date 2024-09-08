35bb8de2ce33a85dd4103b671452366e49d513a9
Tanya-abi <tchigwda6@gmail.com>
2024-09-08 16:51:23 +0200

    React-app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
-      <UserProfile name="Tanya" age="27" bio="Loves good music and hiking"/>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
     </>
   )
 }
