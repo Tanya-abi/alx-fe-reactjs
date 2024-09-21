27e42b02604531018e982d46c5f4a8eb493994d1
Tanya-abi <tchigwda6@gmail.com>
2024-09-21 20:37:38 +0200

    Tailwind

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -14,7 +14,7 @@ const recipesList = () => {
     return (
         <div className="container mx-auto p-4">
             <h1 className=" text-3xl font-bold text-center mb-6">Recipe List</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {recipes.map(recipe => (
                 <div key={recipe.id} className="bg-white shadow-md rounded-lg p-4 transform transition duration-300 hover:shadow-lg hover:scale-105">
                 <h2 className="text-xl font-semibold mb-2">{recipe.title}</h2>
